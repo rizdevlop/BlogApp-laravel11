477fe9d3702fe3acee0daa3721ae896bac531a43
mmhd rznl <nalahyaa@gmail.com>
2024-10-16 21:25:18 +0700

    Matkul RPL Laravel 11

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+});
+
+Route::get('/posts/{post:slug}', function (Post $post) {
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/about', function () {
+    return view('about', ['title' => 'About']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
+});
